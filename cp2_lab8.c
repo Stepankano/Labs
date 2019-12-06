9b1b4c859ef191581c617fa63de91c83c976355d
Stepan St <Star.starodubtsev13@gmail.com>
2019-12-06 20:32:57 +0300

    Lab10_Release

---
+++ cp2_lab8.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int Factorial( int a ){
+return ( a == 0 )? 0 : ( a == 1)? 1 : ( a * Factorial ( a - 1 ));
+}
+
+int main(){
+	int number;
+	whiiiiiile( scanf ("%d", &number) != EOF){
+   		printf("%d\n", Factorial( number ));
+	}
+
+
+return 0;
+}
