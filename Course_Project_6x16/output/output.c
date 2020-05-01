e72006c483f4f51b5e8813ab4cf3bb81236fbd9a
Stepan St <Star.starodubtsev13@gmail.com>
2020-05-02 01:46:28 +0300

    Release

--- Course_Project_6x16/output/output.c
+++ Course_Project_6x16/output/output.c
@@ -14,7 +14,7 @@ void output(info_stud *tmp)  //info_stud
 
     for (;;)
     {
-        printf("%20s %c.%c. %s  ", tmp->fnp.last_name, tmp->fnp.first_name, tmp->fnp.patronymic, (tmp->sex == F) ? "F" : "M");
+        printf("%20s %c.%c. %s  ", tmp->fnp_stud.last_name, tmp->fnp_stud.first_name, tmp->fnp_stud.patronymic, (tmp->sex_stud == F_stud) ? "F" : "M");
         printf("%5d%4d %4d  %3d", tmp->group_num, tmp->mks.off.this_la, tmp->mks.off.this_el, tmp->mks.off.this_lab);
         printf(" %3d %3d %3d %3d\n", tmp->mks.ez.ma, tmp->mks.ez.hi, tmp->mks.ez.dm, tmp->mks.ez.cs);
         if (tmp->next)
@@ -58,8 +58,8 @@ void output_exam(info_exam *tmp)  //info_exam
 
     for (;;)
     {
-        printf("%20s %c.%c. %s  ", tmp->full_name.last_name, tmp->full_name.first_name, tmp->full_name.patronymic, (tmp->sex == F) ? "F" : "M");
-        printf("%6d%4d %3d  %3d %4d", tmp->school_number, tmp->mar.math, tmp->mar.rus, tmp->mar.inf, tmp->mar.phis);
+        printf("%20s %c.%c. %s  ", tmp->fnp_exam.last_name, tmp->fnp_exam.first_name, tmp->fnp_exam.patronymic, (tmp->sex_exam == F_exam) ? "F" : "M");
+        printf("%6d%4d %3d  %3d %4d", tmp->school_number, tmp->mar_exam.math, tmp->mar_exam.rus, tmp->mar_exam.inf, tmp->mar_exam.phis);
         printf(" %5d %5d\n", (tmp->this_medal == 1)?1:0, (tmp->this_essay == 1)?1:0);
         if (tmp->next)
         {
@@ -80,7 +80,7 @@ void output_passenger(info_passenger *tmp)  //info_passenger
 
     for (;;)
     {
-        printf("%20s %c.%c. %3d  ", tmp->full_name.last_name, tmp->full_name.first_name, tmp->full_name.patronymic, tmp->bags);
+        printf("%20s %c.%c. %3d  ", tmp->fnp_pass.last_name, tmp->fnp_pass.first_name, tmp->fnp_pass.patronymic, tmp->bags);
         printf("%4d %20d %5d  %7s %14d\n", tmp->mass, tmp->point, tmp->time, (tmp->this_transit ==1)?"yes":"no", tmp->children_count);
         if (tmp->next)
         {
@@ -101,7 +101,7 @@ void output_school(info_school *tmp)  //info_school
 
     for (;;)
     {
-        printf("%20s %c.%c. %s  ", tmp->full_name.last_name, tmp->full_name.first_name, tmp->full_name.patronymic, (tmp->this_sex == F) ? "F" : "M");
+        printf("%20s %c.%c. %s  ", tmp->fnp_sch.last_name, tmp->fnp_sch.first_name, tmp->fnp_sch.patronymic, (tmp->this_sex == F_sch) ? "F" : "M");
         printf("%5d%3c %20s %20s %20s", tmp->class_num, tmp->lit_class, tmp->University, tmp->work, tmp->army);
         
         if (tmp->next)
