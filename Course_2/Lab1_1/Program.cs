5ce777188c05d91e2633ad284d95bf4adf05b9a2
Stepan St <Star.starodubtsev13@gmail.com>
2020-09-18 20:18:01 +0300

    Release

---
+++ Course_2/Lab1_1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Lab1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewClass a = new NewClass("aaaa");
+            NewClass b = new NewClass("bbbb");
+            NewClass c = new NewClass("cccc");
+
+            System.Console.WriteLine(NewClass.Static_return(a));
+            NewClass.Static_non_return(a);
+            b.Non_static_non_return();
+            System.Console.WriteLine(c.Non_static_return());
+
+        }
+
+    }
+
+     class NewClass
+    {
+        private string Str;
+        static public string Static_return(NewClass Str)
+        {
+            return Str.Str;
+        }
+        static public void Static_non_return(NewClass Str)
+        {
+            System.Console.WriteLine(Str.Str);
+        }
+        public string Non_static_return()
+        {
+            return Str;
+        }
+        public void Non_static_non_return()
+        {
+            System.Console.WriteLine(Str);
+        }
+        public NewClass(String new_str)
+        {
+            Str = new string(new_str);
+        }
+    }
+}
+
---
+++ Course_2/Lab1_2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph A = new Graph<>();
+            Graph B = new Graph<>();
+            Graph K = new Graph<>();
+
+            A.Add(B);
+            A.Add(K);
+
+            Graph J = new Graph<>();
+            Graph D = new Graph<>();
+            Graph E = new Graph<>();
+            Graph F = new Graph<>();
+
+            K.Add(J);
+            B.Add(D);
+            B.Add(E);
+            B.Add(F);
+        }
+    }
+
+    class Graph
+    {
+        private List<Graph> Childs;
+        public void Add(Graph new_child)
+        {
+            Childs.Add(new_child);
+        }
+        public Graph()
+        {
+            Childs.Clear();
+        }
+    }
+}
