ad32ba1cc0e1cb8d3c0eef5ae53f6c7278f5b0f2
Stepan St <Star.starodubtsev13@gmail.com>
2020-09-23 21:48:16 +0300

    Release

---
+++ Course_2/Lab2_1/Aggregation_by_value.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+            ///// begin of monster
+            Graph A = new Graph(
+                0, //a
+               new Graph[2]{
+                new Graph(
+                    1, //k
+                   new Graph[1]{
+                       new Graph(
+                           3, //j
+                           new Graph[0]{})}
+                ),
+                new Graph(
+                    2, //b
+                    new Graph[3]{
+                        new Graph(
+                            4, //d
+                            new Graph[0]{}),
+                        new Graph(
+                            5, //e
+                            new Graph[0]{}),
+                        new Graph(
+                            6, //f
+                            new Graph[0]{})
+                    }
+                )
+               }
+            );
+            ///// end of monster
+            System.Console.WriteLine($"1 A");
+            for (int i = 1; i < 7; i++)
+            {
+                A.MoveTo(i, new char[7] { 'A', 'K', 'B', 'J', 'D', 'E', 'F' });
+            }
+        }
+
+    }
+
+    class Graph
+    {
+        private int Value;
+        private List<Graph> Childs;
+        public void Add(Graph new_child)
+        {
+            Childs.Add(new_child);
+        }
+        public void Add(Graph[] new_child)
+        {
+            foreach (var item in new_child)
+            {
+                Childs.Add(item);
+            }
+        }
+        public Graph(int value)
+        {
+            Childs = new List<Graph>();
+            Value = value;
+        }
+        public Graph(int value, Graph[] new_childs)
+        {
+            Childs = new List<Graph>();
+            Value = value;
+            this.Add(new_childs);
+        }
+        public void MoveTo(int i, char[] ch)
+        {
+
+            System.Console.Write($"{this.Value + 1} => ");
+            foreach (Graph k in Childs)
+            {
+                if (k.Value == i)
+                {
+                    System.Console.WriteLine($"{k.Value + 1} {ch[i]}");
+                    return;
+                }
+                foreach (Graph h in k.Childs)
+                {
+                    if (h.Value == i)
+                    {
+                        System.Console.WriteLine($"{k.Value + 1} => {h.Value + 1} {ch[i]}");
+                        return;
+                    }
+                }
+            }
+
+        }
+    }
+
+}
---
+++ Course_2/Lab2_2/Aggregation_by_attachment.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Lab2
+{
+    class Programm
+    {
+        static void Main(string[] args)
+        {
+
+            Graph_A A = new Graph_A(new int[7] { 1, 2, 3, 4, 5, 6, 7 });
+            System.Console.WriteLine($"1 A");
+            A.MoveTo('K');
+            A.MoveTo('B');
+            A.MoveTo('J');
+            A.MoveTo('D');
+            A.MoveTo('E');
+            A.MoveTo('F');
+        }
+
+    }
+
+    class Graph_A
+    {
+        private int Value;
+        public Graph_A(int[] value)
+        {
+            Value = value[0];
+            B = new Graph_B(value);
+            K = new Graph_K(value);
+        }
+        ~Graph_A() { }
+        private Graph_B B = null;
+        private Graph_K K = null;
+        class Graph_B
+        {
+            private int Value;
+            public Graph_B(int[] value)
+            {
+                Value = value[2];
+                D = new Graph_D(value);
+                E = new Graph_E(value);
+                F = new Graph_F(value);
+            }
+            private Graph_D D = null;
+            private Graph_E E = null;
+            private Graph_F F = null;
+            class Graph_D
+            {
+                private int Value;
+                public Graph_D(int[] value)
+                {
+                    Value = value[4];
+                }
+                public void MoveTo(char name)
+                {
+                    System.Console.WriteLine($"{Value} D");
+                }
+            }
+            class Graph_E
+            {
+                private int Value;
+                public Graph_E(int[] value)
+                {
+                    Value = value[5];
+                }
+                public void MoveTo(char name)
+                {
+                    System.Console.WriteLine($"{Value} E");
+                }
+            }
+            class Graph_F
+            {
+                private int Value;
+                public Graph_F(int[] value)
+                {
+                    Value = value[6];
+                }
+                public void MoveTo(char name)
+                {
+                    System.Console.WriteLine($"{Value} F");
+                }
+            }
+            public void MoveTo(char name)
+            {
+                if (name == 'D')
+                {
+                    System.Console.Write($"{Value} => ");
+                    this.D.MoveTo(name);
+                }
+                else if (name == 'E')
+                {
+                    System.Console.Write($"{Value} => ");
+                    this.E.MoveTo(name);
+                }
+                else if (name == 'F')
+                {
+                    System.Console.Write($"{Value} => ");
+                    this.F.MoveTo(name);
+                }
+                else
+                {
+                    System.Console.WriteLine($"{Value} B");
+                }
+            }
+        }
+        class Graph_K
+        {
+            private int Value;
+            public int MoveTo()
+            {
+
+                return Value;
+            }
+            public Graph_K(int[] value)
+            {
+                Value = value[1];
+                J = new Graph_J(value);
+            }
+            public void MoveTo(char name)
+            {
+                if (name == 'K')
+                {
+                    System.Console.WriteLine($"{Value} K");
+                }
+                else
+                {
+                    System.Console.Write($"{Value} => ");
+                    this.J.MoveTo(name);
+                }
+            }
+            private Graph_J J = null;
+            class Graph_J
+            {
+                private int Value;
+                public Graph_J(int[] value)
+                {
+                    Value = value[3];
+                }
+                public void MoveTo(char name)
+                {
+                    System.Console.WriteLine($"{Value} J");
+                }
+            }
+
+        }
+        public void MoveTo(char name)
+        {
+            System.Console.Write($"{Value} => ");
+            if (name == 'K' || name == 'J')
+            {
+                this.K.MoveTo(name);
+            }
+            else
+            {
+                this.B.MoveTo(name);
+            }
+
+        }
+    }
+}
