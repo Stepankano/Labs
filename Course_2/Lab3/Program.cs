8a359348c7af0d800b512946b5a699427ad6e1cc
Stepan St <Star.starodubtsev13@gmail.com>
2020-10-02 14:58:58 +0300

    Release

---
+++ Course_2/Lab3/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F ff = new F(5);
+            F df = new D(5);
+            F gf = new G(5);
+
+            D dd = new D(5);
+            D gd = new G(5);
+
+            G gg = new G(5);
+
+            System.Console.WriteLine($"ff = {ff.get_value()}");
+            System.Console.WriteLine($"df = {df.get_value()}");
+            System.Console.WriteLine($"gf = {gf.get_value()}");
+            System.Console.WriteLine($"dd = {dd.get_value()}");
+            System.Console.WriteLine($"gd = {gd.get_value()}");
+            System.Console.WriteLine($"gg = {gg.get_value()}");
+
+        }
+    }
+    class F
+    {
+        protected int value = 0;
+        public F(int a)
+        {
+            System.Console.WriteLine($"It`s {this.ToString()}");
+            value = a;
+        }
+        public virtual int get_value()
+        {
+            return value;
+        }
+    }
+    class D : F
+    {
+        public D(int a) : base(a) { }
+        public override int get_value()
+        {
+            return base.get_value() + 10;
+        }
+    }
+    sealed class G : D
+    {
+        public G(int a) : base(a) { }
+        public override int get_value()
+        {
+            return base.get_value() + 100;
+        }
+    }
+}
---
+++ Course_2/Lab4_1/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine($"Lab4.A : \nЭто абстрактный класс");
+            B b = new B();
+            K k = new K();
+            D d = new D();
+            F f = new F();
+        }
+    }
+
+    abstract class A
+    {
+
+        public void A_func()
+        {
+            System.Console.WriteLine("Afunc");
+        }
+        protected int A_int_func()
+        {
+            return 1;
+        }
+    }
+    class B : A
+    {
+        public B()
+        {
+            System.Console.WriteLine($"{this.ToString()} : ");
+            B_func();
+
+        }
+        protected virtual void B_func()
+        {
+            System.Console.WriteLine("Это расширение по функции");
+        }
+    }
+    class K : A
+    {
+        public K()
+        {
+            System.Console.WriteLine($"{this.ToString()} : ");
+            str = "Это расширение по полю";
+            System.Console.WriteLine(str);
+        }
+        private string str = null;
+    }
+    class D : B
+    {
+        public D() : base() { }
+        protected override void B_func()
+        {
+            System.Console.WriteLine("Это специализация");
+        }
+    }
+    public interface J
+    {
+        void begin();
+    }
+    public interface E
+    {
+        void of_phrase();
+    }
+    class F : D, J, E   // ко всему классу применено всё и сразу (конструирование)
+    {
+        public F()
+        {
+            System.Console.WriteLine($"{this.ToString()} : ");
+            begin();
+            of_phrase();
+            System.Console.WriteLine($"Это комбинирование и спецификация J и E интерфейсов");
+            void_func();
+            B_func();
+        }
+        protected override void B_func()
+        {
+            System.Console.WriteLine("И специализация тоже");
+        }
+        public void begin()
+        {
+            System.Console.Write("\"Начало "); //это спецификация
+        }
+        public void of_phrase()
+        {
+            System.Console.WriteLine("фразы\""); //тоже спецификация
+        }
+        private int new_value = 12; // это расширение по полю
+        private void void_func()
+        {
+            System.Console.WriteLine($"И расширение (value = {new_value})");
+        } // это расширение по функции
+    }
+}
