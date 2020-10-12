c3f338446448e420980089e61aa1ab3ebfc8e837
Stepan St <Star.starodubtsev13@gmail.com>
2020-10-12 13:07:33 +0300

    Release

---
+++ Course_2/Lab7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Lab7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client clnt = new Client(90);
+            Server srvr = new Server();
+            System.Console.WriteLine($"Server.Value = {srvr.get_b()}");
+            System.Console.WriteLine($"Client.Value = {srvr.get_get_value(clnt)}");
+        }
+    }
+    class Client
+    {
+        public Client(int a) => value = a;
+
+        private int value = 0;
+        public int get_value() => value;
+
+    }
+    class Server
+    {
+        public int get_get_value(Client a) => a.get_value();
+        private int b = 100;
+        public int get_b() => b;
+    }
+
+
+}
+
---
+++ Course_2/Lab8/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Template<int> intr = new Template<int>(100);
+            Template<string> str = new Template<string>("string");
+            Template<Template<int>> TT = new Template<Template<int>>(intr);
+            intr.print_value();
+            str.print_value();
+            TT.print_value();
+            int int1 = 1, int2 = 2;
+            System.Console.WriteLine($"int1 = {int1}, int 2 = {int2}");
+            Template<int>.swap(ref int1,ref int2);
+            System.Console.WriteLine($"int1 = {int1}, int 2 = {int2}");
+        }
+    }
+    class Template<T>
+    {
+        private T value;
+        public Template(T arg)
+        {
+            value = arg;
+            if (arg is int)
+            {
+                System.Console.WriteLine("New int object!");
+                return;
+            }
+            if (arg is string)
+            {
+                System.Console.WriteLine("New string object!");
+                return;
+            }
+            if (arg is Template<int>)
+            {
+                System.Console.WriteLine("New Template<int> object!");
+                return;
+            }
+            else
+            {
+                System.Console.WriteLine("I dont know what is it");
+            }
+        }
+        public void print_value() =>
+            System.Console.WriteLine($"{this.ToString()} = {this.value}");
+        public static void swap(ref T frst, ref T scnd)
+        {
+            T third = frst;
+            frst = scnd;
+            scnd = third;
+            System.Console.WriteLine("Swap!");
+        }
+
+    }
+}
